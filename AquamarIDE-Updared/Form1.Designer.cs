b299dcc37dc74d86a51bc3bb43c6e849f8fecd75
Aquamarine Hoshino <cozoral@outlook.com>
2025-03-08 12:41:28 +0100

    More support
    Update UI components and package versions
    
    - Updated `AquamarIDE-Updated.csproj` to use newer versions of `EO.WebBrowser` and `Microsoft.Web.WebView2`.
    - Added new UI components in `Form1.Designer.cs`, including context menus for copy/paste and a list box for file display.
    - Enhanced `Form1.cs` with new methods for file loading/saving and context menu operations, along with corresponding event handlers.
    - Updated `Form1.resx` to include metadata for new context menus and dialogs.
    - Overall improvements enhance user interaction and file management capabilities.
    -Now supports multiple files.
    -Can now open folders.

--- AquamarIDE-Updared/Form1.Designer.cs
+++ AquamarIDE-Updared/Form1.Designer.cs
@@ -31,6 +31,9 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             richTextBox1 = new RichTextBox();
+            contextMenuStrip2 = new ContextMenuStrip(components);
+            copyToolStripMenuItem1 = new ToolStripMenuItem();
+            pasteToolStripMenuItem1 = new ToolStripMenuItem();
             openFileDialog1 = new OpenFileDialog();
             saveFileDialog1 = new SaveFileDialog();
             colorDialog1 = new ColorDialog();
@@ -43,35 +46,73 @@
             menuStrip1 = new MenuStrip();
             fileToolStripMenuItem1 = new ToolStripMenuItem();
             loadToolStripMenuItem1 = new ToolStripMenuItem();
+            loadFileToolStripMenuItem = new ToolStripMenuItem();
+            loadFolerToolStripMenuItem = new ToolStripMenuItem();
             saveToolStripMenuItem1 = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem2 = new ToolStripMenuItem();
             clipboardToolStripMenuItem = new ToolStripMenuItem();
             copyToolStripMenuItem = new ToolStripMenuItem();
             pasteToolStripMenuItem = new ToolStripMenuItem();
             cosmeticToolStripMenuItem = new ToolStripMenuItem();
-            changeColorToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
             changeFontToolStripMenuItem = new ToolStripMenuItem();
-            changeBackgroundColorToolStripMenuItem = new ToolStripMenuItem();
             debugToolStripMenuItem = new ToolStripMenuItem();
             previewHTMLToolStripMenuItem = new ToolStripMenuItem();
+            listBox1 = new ListBox();
+            contextMenuStrip3 = new ContextMenuStrip(components);
+            dToolStripMenuItem = new ToolStripMenuItem();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            label1 = new Label();
+            contextMenuStrip2.SuspendLayout();
             contextMenuStrip1.SuspendLayout();
             menuStrip1.SuspendLayout();
+            contextMenuStrip3.SuspendLayout();
             SuspendLayout();
             // 
             // richTextBox1
             // 
-            richTextBox1.BackColor = Color.FromArgb(64, 64, 64);
-            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.BackColor = Color.FromArgb(32, 32, 32);
+            richTextBox1.ContextMenuStrip = contextMenuStrip2;
             richTextBox1.EnableAutoDragDrop = true;
-            richTextBox1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
             richTextBox1.ForeColor = Color.FromArgb(224, 224, 224);
-            richTextBox1.Location = new Point(0, 0);
-            richTextBox1.Margin = new Padding(3, 100, 3, 3);
+            richTextBox1.Location = new Point(256, 30);
+            richTextBox1.Margin = new Padding(3, 133, 3, 4);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.ShowSelectionMargin = true;
-            richTextBox1.Size = new Size(1904, 1041);
-            richTextBox1.TabIndex = 0;
+            richTextBox1.Size = new Size(1920, 1358);
+            richTextBox1.TabIndex = 2;
             richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
             richTextBox1.DoubleClick += richTextBox1_DoubleClick;
+            richTextBox1.KeyDown += richTextBox1_KeyDown;
+            richTextBox1.KeyUp += richTextBox1_KeyUp;
+            // 
+            // contextMenuStrip2
+            // 
+            contextMenuStrip2.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip2.Items.AddRange(new ToolStripItem[] { copyToolStripMenuItem1, pasteToolStripMenuItem1 });
+            contextMenuStrip2.Name = "contextMenuStrip2";
+            contextMenuStrip2.Size = new Size(113, 52);
+            contextMenuStrip2.Text = "AquamarIDE";
+            contextMenuStrip2.Opening += contextMenuStrip2_Opening;
+            // 
+            // copyToolStripMenuItem1
+            // 
+            copyToolStripMenuItem1.Name = "copyToolStripMenuItem1";
+            copyToolStripMenuItem1.Size = new Size(112, 24);
+            copyToolStripMenuItem1.Text = "Copy";
+            copyToolStripMenuItem1.Click += copyToolStripMenuItem1_Click;
+            // 
+            // pasteToolStripMenuItem1
+            // 
+            pasteToolStripMenuItem1.Name = "pasteToolStripMenuItem1";
+            pasteToolStripMenuItem1.Size = new Size(112, 24);
+            pasteToolStripMenuItem1.Text = "Paste";
+            pasteToolStripMenuItem1.Click += pasteToolStripMenuItem1_Click;
             // 
             // openFileDialog1
             // 
@@ -84,34 +125,37 @@
             // 
             // contextMenuStrip1
             // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
             contextMenuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, loadToolStripMenuItem, saveToolStripMenuItem });
             contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new Size(101, 70);
+            contextMenuStrip1.Size = new Size(112, 76);
             // 
             // fileToolStripMenuItem
             // 
             fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            fileToolStripMenuItem.Size = new Size(100, 22);
+            fileToolStripMenuItem.Size = new Size(111, 24);
             fileToolStripMenuItem.Text = "File";
             // 
             // loadToolStripMenuItem
             // 
             loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-            loadToolStripMenuItem.Size = new Size(100, 22);
+            loadToolStripMenuItem.Size = new Size(111, 24);
             loadToolStripMenuItem.Text = "Load";
             // 
             // saveToolStripMenuItem
             // 
             saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            saveToolStripMenuItem.Size = new Size(100, 22);
+            saveToolStripMenuItem.Size = new Size(111, 24);
             saveToolStripMenuItem.Text = "Save";
             // 
             // menuStrip1
             // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
             menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem1, cosmeticToolStripMenuItem, debugToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(1904, 24);
+            menuStrip1.Padding = new Padding(7, 3, 0, 3);
+            menuStrip1.Size = new Size(2176, 30);
             menuStrip1.TabIndex = 8;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -119,101 +163,181 @@
             // 
             fileToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { loadToolStripMenuItem1, saveToolStripMenuItem1, clipboardToolStripMenuItem });
             fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
-            fileToolStripMenuItem1.Size = new Size(37, 20);
+            fileToolStripMenuItem1.Size = new Size(46, 24);
             fileToolStripMenuItem1.Text = "File";
             // 
             // loadToolStripMenuItem1
             // 
+            loadToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { loadFileToolStripMenuItem, loadFolerToolStripMenuItem });
             loadToolStripMenuItem1.Name = "loadToolStripMenuItem1";
-            loadToolStripMenuItem1.Size = new Size(126, 22);
+            loadToolStripMenuItem1.Size = new Size(158, 26);
             loadToolStripMenuItem1.Text = "Load";
             loadToolStripMenuItem1.Click += loadToolStripMenuItem1_Click;
             // 
+            // loadFileToolStripMenuItem
+            // 
+            loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
+            loadFileToolStripMenuItem.Size = new Size(169, 26);
+            loadFileToolStripMenuItem.Text = "Load file";
+            loadFileToolStripMenuItem.Click += loadFileToolStripMenuItem_Click;
+            // 
+            // loadFolerToolStripMenuItem
+            // 
+            loadFolerToolStripMenuItem.Name = "loadFolerToolStripMenuItem";
+            loadFolerToolStripMenuItem.Size = new Size(169, 26);
+            loadFolerToolStripMenuItem.Text = "Load folder";
+            loadFolerToolStripMenuItem.Click += loadFolerToolStripMenuItem_Click;
+            // 
             // saveToolStripMenuItem1
             // 
+            saveToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { saveAsToolStripMenuItem, saveToolStripMenuItem2 });
             saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
-            saveToolStripMenuItem1.Size = new Size(126, 22);
+            saveToolStripMenuItem1.Size = new Size(158, 26);
             saveToolStripMenuItem1.Text = "Save";
             saveToolStripMenuItem1.Click += saveToolStripMenuItem1_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(141, 26);
+            saveAsToolStripMenuItem.Text = "Save as";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem2
+            // 
+            saveToolStripMenuItem2.Name = "saveToolStripMenuItem2";
+            saveToolStripMenuItem2.Size = new Size(141, 26);
+            saveToolStripMenuItem2.Text = "Save";
+            saveToolStripMenuItem2.Click += saveToolStripMenuItem2_Click;
             // 
             // clipboardToolStripMenuItem
             // 
             clipboardToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { copyToolStripMenuItem, pasteToolStripMenuItem });
             clipboardToolStripMenuItem.Name = "clipboardToolStripMenuItem";
-            clipboardToolStripMenuItem.Size = new Size(126, 22);
+            clipboardToolStripMenuItem.Size = new Size(158, 26);
             clipboardToolStripMenuItem.Text = "Clipboard";
             // 
             // copyToolStripMenuItem
             // 
             copyToolStripMenuItem.Name = "copyToolStripMenuItem";
-            copyToolStripMenuItem.Size = new Size(102, 22);
+            copyToolStripMenuItem.Size = new Size(126, 26);
             copyToolStripMenuItem.Text = "Copy";
             copyToolStripMenuItem.Click += copyToolStripMenuItem_Click;
             // 
             // pasteToolStripMenuItem
             // 
             pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
-            pasteToolStripMenuItem.Size = new Size(102, 22);
+            pasteToolStripMenuItem.Size = new Size(126, 26);
             pasteToolStripMenuItem.Text = "Paste";
             pasteToolStripMenuItem.Click += pasteToolStripMenuItem_Click;
             // 
             // cosmeticToolStripMenuItem
             // 
-            cosmeticToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { changeColorToolStripMenuItem, changeFontToolStripMenuItem, changeBackgroundColorToolStripMenuItem });
+            cosmeticToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem2, changeFontToolStripMenuItem });
             cosmeticToolStripMenuItem.Name = "cosmeticToolStripMenuItem";
-            cosmeticToolStripMenuItem.Size = new Size(69, 20);
+            cosmeticToolStripMenuItem.Size = new Size(84, 24);
             cosmeticToolStripMenuItem.Text = "Cosmetic";
             // 
-            // changeColorToolStripMenuItem
+            // toolStripMenuItem1
             // 
-            changeColorToolStripMenuItem.Name = "changeColorToolStripMenuItem";
-            changeColorToolStripMenuItem.Size = new Size(212, 22);
-            changeColorToolStripMenuItem.Text = "Change color";
-            changeColorToolStripMenuItem.Click += changeColorToolStripMenuItem_Click;
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(271, 26);
+            toolStripMenuItem1.Text = "Change Color";
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click_1;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(271, 26);
+            toolStripMenuItem2.Text = "Change Background colour";
+            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
             // 
             // changeFontToolStripMenuItem
             // 
             changeFontToolStripMenuItem.Name = "changeFontToolStripMenuItem";
-            changeFontToolStripMenuItem.Size = new Size(212, 22);
+            changeFontToolStripMenuItem.Size = new Size(271, 26);
             changeFontToolStripMenuItem.Text = "Change font";
             changeFontToolStripMenuItem.Click += changeFontToolStripMenuItem_Click;
-            // 
-            // changeBackgroundColorToolStripMenuItem
-            // 
-            changeBackgroundColorToolStripMenuItem.Name = "changeBackgroundColorToolStripMenuItem";
-            changeBackgroundColorToolStripMenuItem.Size = new Size(212, 22);
-            changeBackgroundColorToolStripMenuItem.Text = "Change background color";
-            changeBackgroundColorToolStripMenuItem.Click += changeBackgroundColorToolStripMenuItem_Click;
             // 
             // debugToolStripMenuItem
             // 
             debugToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { previewHTMLToolStripMenuItem });
             debugToolStripMenuItem.Name = "debugToolStripMenuItem";
-            debugToolStripMenuItem.Size = new Size(54, 20);
+            debugToolStripMenuItem.Size = new Size(68, 24);
             debugToolStripMenuItem.Text = "Debug";
             // 
             // previewHTMLToolStripMenuItem
             // 
             previewHTMLToolStripMenuItem.Name = "previewHTMLToolStripMenuItem";
-            previewHTMLToolStripMenuItem.Size = new Size(156, 22);
+            previewHTMLToolStripMenuItem.Size = new Size(192, 26);
             previewHTMLToolStripMenuItem.Text = "Preview(HTML)";
             previewHTMLToolStripMenuItem.Click += previewHTMLToolStripMenuItem_Click;
             // 
+            // listBox1
+            // 
+            listBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            listBox1.BackColor = Color.FromArgb(64, 64, 64);
+            listBox1.ContextMenuStrip = contextMenuStrip3;
+            listBox1.ForeColor = SystemColors.Window;
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(0, 30);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(259, 1344);
+            listBox1.TabIndex = 9;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // contextMenuStrip3
+            // 
+            contextMenuStrip3.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip3.Items.AddRange(new ToolStripItem[] { dToolStripMenuItem });
+            contextMenuStrip3.Name = "contextMenuStrip3";
+            contextMenuStrip3.Size = new Size(123, 28);
+            contextMenuStrip3.Text = "Files";
+            contextMenuStrip3.Opening += contextMenuStrip3_Opening;
+            // 
+            // dToolStripMenuItem
+            // 
+            dToolStripMenuItem.Name = "dToolStripMenuItem";
+            dToolStripMenuItem.Size = new Size(122, 24);
+            dToolStripMenuItem.Text = "Delete";
+            dToolStripMenuItem.Click += dToolStripMenuItem_Click;
+            // 
+            // folderBrowserDialog1
+            // 
+            folderBrowserDialog1.HelpRequest += folderBrowserDialog1_HelpRequest;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(75, 1368);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 20);
+            label1.TabIndex = 10;
+            label1.Text = "AquamarIDE";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkSlateGray;
-            ClientSize = new Size(1904, 1041);
+            ClientSize = new Size(2176, 1388);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
             Controls.Add(menuStrip1);
             Controls.Add(richTextBox1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = menuStrip1;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "AquamarIDE";
+            TransparencyKey = Color.DarkSlateGray;
+            contextMenuStrip2.ResumeLayout(false);
             contextMenuStrip1.ResumeLayout(false);
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            contextMenuStrip3.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -235,13 +359,26 @@
         private ToolStripMenuItem loadToolStripMenuItem1;
         private ToolStripMenuItem saveToolStripMenuItem1;
         private ToolStripMenuItem cosmeticToolStripMenuItem;
-        private ToolStripMenuItem changeColorToolStripMenuItem;
         private ToolStripMenuItem changeFontToolStripMenuItem;
-        private ToolStripMenuItem changeBackgroundColorToolStripMenuItem;
+       
         private ToolStripMenuItem debugToolStripMenuItem;
         private ToolStripMenuItem previewHTMLToolStripMenuItem;
         private ToolStripMenuItem clipboardToolStripMenuItem;
         private ToolStripMenuItem copyToolStripMenuItem;
         private ToolStripMenuItem pasteToolStripMenuItem;
+        private ListBox listBox1;
+        private ToolStripMenuItem loadFileToolStripMenuItem;
+        private ToolStripMenuItem loadFolerToolStripMenuItem;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private Label label1;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem2;
+        private ContextMenuStrip contextMenuStrip2;
+        private ToolStripMenuItem copyToolStripMenuItem1;
+        private ToolStripMenuItem pasteToolStripMenuItem1;
+        private ContextMenuStrip contextMenuStrip3;
+        private ToolStripMenuItem dToolStripMenuItem;
     }
 }
--- AquamarIDE-Updared/Form1.cs
+++ AquamarIDE-Updared/Form1.cs
@@ -2,11 +2,18 @@ using System.Text;
 
 namespace AquamarIDE_Updared
 {
+
     public partial class Form1 : Form
     {
+
+        public string[] files;
+        public bool IsFolder = false;
         public Form1()
         {
             InitializeComponent();
+            listBox1.Visible = false;
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -15,25 +22,25 @@ namespace AquamarIDE_Updared
         }
         public void ReadTextFile(string path)
         {
-          /*  using (StreamReader reader = new StreamReader(path))
-            {
-                {
-                    string line;
-                    StringBuilder builder = new StringBuilder();
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        builder.Append(line);
-                    }
-                    reader.Close();
-                    richTextBox1.Text = builder.ToString();
+            /*  using (StreamReader reader = new StreamReader(path))
+              {
+                  {
+                      string line;
+                      StringBuilder builder = new StringBuilder();
+                      while ((line = reader.ReadLine()) != null)
+                      {
+                          builder.Append(line);
+                      }
+                      reader.Close();
+                      richTextBox1.Text = builder.ToString();
 
-                }
-            }*/
+                  }
+              }*/
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-             
+
         }
 
 
@@ -69,20 +76,12 @@ namespace AquamarIDE_Updared
 
         private void loadToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Multiselect = true;
-            if (fileDialog.ShowDialog() == DialogResult.OK)
-            {
-                richTextBox1.Text = fileDialog.FileName;
-                MessageBox.Show(fileDialog.SafeFileName + " has been loaded");
-                 string content = File.ReadAllText(fileDialog.FileName);
-                richTextBox1.Text = content;
-            }
+
         }
 
         private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            saveFileDialog1.ShowDialog();
+
         }
 
         private void changeColorToolStripMenuItem_Click(object sender, EventArgs e)
@@ -129,5 +128,166 @@ namespace AquamarIDE_Updared
         {
             richTextBox1.Text = Clipboard.GetText();
         }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void loadFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = fileDialog.FileName;
+                MessageBox.Show(fileDialog.SafeFileName + " has been loaded");
+                string content = File.ReadAllText(fileDialog.FileName);
+                richTextBox1.Text = content;
+
+            }
+        }
+
+        private void loadFolerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog.SelectedPath;
+                MessageBox.Show(folderPath + " has been loaded");
+                listBox1.Items.Clear();
+                files = Directory.GetFiles(folderPath);
+                IsFolder = true;
+                listBox1.Visible = true;
+
+                foreach (var file in files)
+                {
+                    var filename = Path.GetFileName(file);
+                    listBox1.Items.Add(filename);
+                }
+            }
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                string selectedFile = files.FirstOrDefault(f => Path.GetFileName(f) == listBox1.SelectedItem.ToString());
+                if (selectedFile != null)
+                {
+                    richTextBox1.Text = File.ReadAllText(selectedFile);
+                }
+            }
+        }
+
+
+        private void toolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            richTextBox1.ForeColor = colorDialog1.Color;
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            colorDialog2.ShowDialog();
+            richTextBox1.BackColor = colorDialog2.Color;
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedFile = files.FirstOrDefault(f => Path.GetFileName(f) == listBox1.SelectedItem.ToString());
+                if (selectedFile != null)
+                {
+                    File.WriteAllText(selectedFile, richTextBox1.Text);
+                }
+            }
+            else
+            {
+                saveFileDialog1.ShowDialog();
+            }
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                saveToolStripMenuItem2_Click(sender, e);
+            }
+            else if (e.KeyCode == Keys.F5)
+            {
+                previewHTMLToolStripMenuItem_Click(sender, e);
+            }
+            //else if (e.KeyCode == Keys.Tab)
+            //{
+            //    richTextBox1.Text = "    ";
+
+            //}
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //if (e.KeyCode == Keys.Tab)
+            //{
+            //    richTextBox1.Text += "    ";
+
+            //}
+        }
+
+        private void contextMenuStrip2_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void copyToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText != "")
+            {
+                Clipboard.SetText(richTextBox1.SelectedText);
+            }
+        }
+
+        private void pasteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += Clipboard.GetText();
+
+        }
+
+        private void contextMenuStrip3_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void dToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedFile = files.FirstOrDefault(f => Path.GetFileName(f) == listBox1.SelectedItem.ToString());
+                if (selectedFile != null)
+                {
+                    File.Delete(selectedFile);
+                    listBox1.Items.Remove(listBox1.SelectedItem);
+                }
+            }
+        }
+
+        private void copyToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+        }
     }
+
 }
